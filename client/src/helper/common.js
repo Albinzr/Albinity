d06b1fb4e6a21f3e2a090d943bb4eba92b6a7644
albinzr <albinzr@gmail.com>
2017-09-27 00:01:39 +0530

    update env bug

--- client/src/helper/common.js
+++ client/src/helper/common.js
@@ -1,10 +1,11 @@
 const mainUrl = () => {
-	switch (process.env.REACT_ENV) {
+	console.log('current env', process.env.REACT_APP_ENV)
+	switch (process.env.REACT_APP_ENV) {
 		case 'production':
-			console.log(process.env.REACT_ENV)
+			console.log(process.env.REACT_APP_ENV)
 			return 'http://albin.in:9000'
 		default:
-			console.log(process.env.REACT_ENV)
+			console.log(process.env.REACT_APP_ENV)
 			return 'http://localhost:9000'
 	}
 }
